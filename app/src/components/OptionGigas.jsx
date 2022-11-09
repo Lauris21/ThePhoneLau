aa22b0ca083bf4452888b1a25d25cf0ff79627b1
Lauris-21 <laurasanchez0301@gmail.com>
2022-11-09 19:54:17 +0100

    Prueba Tecnica

--- app/src/components/OptionGigas.jsx
+++ app/src/components/OptionGigas.jsx
@@ -4,13 +4,22 @@ import { DataContext } from '../context/DataContext';
 const OptionGigas = () => {
   const { phones } = useContext(DataContext);
 
+  const orden = (a, b) => {
+    if (a < b) {
+      return -1;
+    }
+    if (a > b) {
+      return 1;
+    }
+  };
+
   const gigas = [];
   const mapeoGigas = phones.map((item) => item.gigas);
   mapeoGigas.forEach((element) => {
     if (!gigas.includes(element)) {
       gigas.push(element);
     }
-    return gigas;
+    return gigas.sort(orden);
   });
   const optionGigas = [];
   gigas.forEach((item) => {
