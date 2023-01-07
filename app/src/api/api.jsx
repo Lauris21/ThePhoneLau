f94c8c1dca263b692bef1ef051c5786c8f4ba0c2
Lauris-21 <laurasanchez0301@gmail.com>
2023-01-07 01:30:18 +0100

    Fetch

--- app/src/api/api.jsx
+++ app/src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const getData = async () => {
   try {
-    const data = await axios.get('http://localhost:8080/phones');
+    const data = await axios.get('https://phone-7fb6.onrender.com/phones');
     return data.data;
   } catch (error) {
     return error;
